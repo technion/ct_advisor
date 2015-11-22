1b0b6f8f7b9f592471e3ce37cd85c1cc5df57955
Technion <technion@lolware.net>
2015-11-22 06:22:41 +0000

    Implemented scheduling service.

--- apps/ct_advisor/src/ct_advisor_app.erl
+++ apps/ct_advisor/src/ct_advisor_app.erl
@@ -14,8 +14,16 @@
 %%====================================================================
 %% API
 %%====================================================================
+setup() ->
+    sth = ets:new(sth, [ named_table, public, {read_concurrency, true}]),
+    STH = ct_fetch:fetch_sth(), % TODO: Use previously saved disk value
+    Latest = ct_fetch:parse_sth(STH),
+    ets:insert(sth, {latest, Latest}),
+    io:fwrite("First lookup: ~B~n", [Latest]).
 
 start(_StartType, _StartArgs) ->
+    setup(),
+    scheduler:start_link(),
     'ct_advisor_sup':start_link().
 
 %%--------------------------------------------------------------------
--- apps/ct_advisor/src/leaf_iterate.erl
+++ apps/ct_advisor/src/leaf_iterate.erl
@@ -1,6 +1,39 @@
 -module(leaf_iterate).
 -compile([debug_info, export_all]).
 
+scheduled_check() ->
+    STH = ct_fetch:fetch_sth(),
+    Latest = ct_fetch:parse_sth(STH),
+    lookup_updates(Latest).
+
+lookup_updates(Latest) ->
+    case ets:lookup(sth, latest) of
+    [{latest, LastLookup}] when Latest > LastLookup ->
+        io:fwrite("Performing checks: ~B~n", [Latest]),
+        Domains = run_checks(LastLookup, Latest),
+        io:fwrite("Domains to verify: ~p~n", [Domains]);
+    _ ->
+        io:fwrite("No updates, latest still: ~B~n", [Latest])
+    end.
+
+run_checks(LOW, HIGH) ->
+    {FROM, TO} = get_range(LOW, HIGH),
+    io:fwrite("Running between: ~B and ~B~n", [FROM, TO]),
+    Domains = enumerate_ids(FROM, TO),
+    ets:insert(sth, {latest, TO+1}),
+    Domains.
+
+get_range(LOW, HIGH) when HIGH > LOW ->
+    % Note the highest lookup should be STH -1
+    % We also rate limit lookups per run
+    case (HIGH - LOW) of
+    Diff when Diff > 32 ->
+        {LOW, LOW+32};
+    _Diff ->
+        {LOW, HIGH-1}
+    end.
+        
+
 get_domain_from_id(ID) ->
     LeafEntry =  ct_fetch:fetch_entry(ID),
     MTL = leaf_parse:parse_leaf(LeafEntry),
@@ -13,3 +46,18 @@ enumerate_ids(ID, ID) ->
 enumerate_ids(FROM, TO) when FROM < TO ->
     [get_domain_from_id(FROM)| enumerate_ids(FROM+1, TO)].
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+setup_table_test() ->
+    sth = ets:new(sth, [ named_table, public, {read_concurrency, true}]),
+    ets:insert(sth, {latest, 1024}). %Generally random starting example
+
+range_test() ->
+    ?assertEqual(get_range(7, 8), {7, 7}),
+    ?assertEqual(get_range(7, 107), {7, 39}).
+
+lookup_test() ->
+    lookup_updates(1025),
+    ?assertEqual(ets:lookup(sth, latest), [{latest,1025}]).
+-endif.
