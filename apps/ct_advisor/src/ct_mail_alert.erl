061f70c3cf52dc7813da3954039617f50ae0df64
Technion <technion@lolware.net>
2015-12-03 08:54:31 +0000

    Updated tests with SES go live.

--- apps/ct_advisor/src/ct_mail_alert.erl
+++ apps/ct_advisor/src/ct_mail_alert.erl
@@ -5,7 +5,7 @@
 
 %% Issues an email alert.
 
--spec send_alert([{string(), string()}], [tuple()]) -> ok.
+-spec send_alert([{string(), string()}], [tuple()]) -> {'ok',pid()}.
 send_alert([{Domain, User}], Certificate) ->
     lager:notice("We have an alert for ~p, ~p with cert ~p~n",
         [Domain, User, Certificate]),
@@ -25,11 +25,10 @@ send_alert([{Domain, User}], Certificate) ->
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
-send_failmail_test() ->
-    % In order to not flood mailboxes, this tests an attempt to send an email.
-    % The test address is invalid - if we get the correct server rseponse
-    % we assume we can succeed.
-    {T, Pid} = send_alert([{"lolwaretest.net", "technion@invalidwillfail.com"}],
+send_bouncemail_test() ->
+    % AWS SES will still accept a bounce - but should generate an alert to SNS
+    {T, Pid} = send_alert([{"lolwaretest.net",
+            "bounce@simulator.amazonses.com"}],
             [{dNSName, "www.lolwaretest.net"}, {dNSName, "lolwaretest.net"}]),
     ?assertEqual(ok, T),
     unlink(Pid),
@@ -40,11 +39,7 @@ send_failmail_test() ->
     after 5000 ->
         nomessage
     end,
-    {error, no_more_hosts, {permanent_failure, _, SMTPError}} = Response,
-    ?assertEqual(
-        <<"554 Message rejected: Email address is not verified.\r\n">>,
-        SMTPError). % TODO: After we actually verify with SES, this
-                    % error will change.
+    ?assertEqual(normal, Response).
 
 send_mail_test() ->
     % In order to not flood mailboxes, this tests an attempt to send an email.
